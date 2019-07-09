14236e5cb422a2e7f3c55b2bf0e4c71eb545c84b
Zenno Bruinsma <zenno.bruinsma@bloomon.nl>
2019-07-09 15:41:43 +0200

    added image with google places api

--- server.js
+++ server.js
@@ -57,6 +57,24 @@ app.get('/map/:lat/:long', (req, res) => {
   }
 });
 
+app.get('/places/names', (req, res) => {
+  googleMapsClient
+    .placesNearby({
+      location: ['52.34899951406097', '4.938355388489091'],
+      radius: 100,
+      name: 't Hoekje'
+    })
+    .asPromise()
+    .then(response => {
+      console.log(response);
+      const { results } = response.json;
+      res.send(results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.get('/search/:search', (req, res) => {
   googleMapsClient
     .geocode({ address: req.params.search })
@@ -91,18 +109,53 @@ app.get('/spottedbylocals', (req, res) => {
       // always executed
     });
 });
-
+var placeData = [];
 app.get('/foursqaure/places/:lat/:long', (req, res) => {
   axios
     .get(
-      `https://api.foursquare.com/v2/venues/explore?client_id=1DHVNT5QG4YE1OLDDST3IXTWZWZ5WSZ1VOCDZBXLEFLQWXER&client_secret=CE4JOEELFV4HZ1TWWUSSFZQXNGHJNCOGMM2SPCAE14E5YWPV&v=20190911&section=food&intent=checkin&radius=200&&ll=${
+      `https://api.foursquare.com/v2/venues/explore?client_id=1DHVNT5QG4YE1OLDDST3IXTWZWZ5WSZ1VOCDZBXLEFLQWXER&client_secret=CE4JOEELFV4HZ1TWWUSSFZQXNGHJNCOGMM2SPCAE14E5YWPV&v=20190911&section=food&intent=checkin&radius=200&ll=${
         req.params.lat
       },${req.params.long}`
     )
     .then(respsonse => {
-      console.log(respsonse.data.response.groups[0].items);
-      res.send(respsonse.data.response.groups[0].items);
+      // res.send(respsonse.data.response.groups[0].items);
+      const { items } = respsonse.data.response.groups[0];
+      let f_places = items.map(item => {
+        return {
+          name: item.venue.name,
+          location: [item.venue.location.lat, item.venue.location.lng]
+        };
+      });
+      let count = f_places.length;
+      f_places.forEach(element => {
+        console.log(count);
+        count -= 1;
+        googleMapsClient
+          .placesNearby({
+            location: element.location,
+            radius: 100,
+            name: element.name
+          })
+          .asPromise()
+          .then(response => {
+            const { results, status } = response.json;
+            var newPlaceData = {
+              info: element,
+              image: results[0] ? results[0].photos[0].photo_reference : null
+            };
+            placeData.push(newPlaceData);
+
+            if (count == 0) {
+              console.log(placeData);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+      res.send(placeData);
     })
+
     .catch(err => {
       res.send(err);
     });
